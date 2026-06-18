43717d064d2bafab7cf4a6343e13708a9bcdc2d6
auau1662fr <auric.chen.2005@gmail.com>
2026-06-18 11:51:31 +0200

    add log_parsing$

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -2,25 +2,17 @@
 import sys
 
 total_size = 0
-status_codes = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
-
 line_count = 0
+status_codes = {}
+
+valid_codes = ['200', '301', '400', '401',
+               '403', '404', '405', '500']
 
 
 def print_stats():
     print("File size: {}".format(total_size))
     for code in sorted(status_codes.keys()):
-        if status_codes[code] != 0:
-            print("{}: {}".format(code, status_codes[code]))
+        print("{}: {}".format(code, status_codes[code]))
 
 
 try:
@@ -29,14 +21,18 @@ try:
 
         try:
             parts = line.split()
+
             status = parts[-2]
             size = int(parts[-1])
 
             total_size += size
 
-            if status in status_codes:
-                status_codes[status] += 1
-        except (IndexError, ValueError):
+            if status in valid_codes:
+                status_codes[status] = (
+                    status_codes.get(status, 0) + 1
+                )
+
+        except (ValueError, IndexError):
             pass
 
         if line_count % 10 == 0:
