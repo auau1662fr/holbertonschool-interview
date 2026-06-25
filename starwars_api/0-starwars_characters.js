4f47f40a8d59b8248fd24a95e327887695d79191
auau1662fr <auric.chen.2005@gmail.com>
2026-06-25 10:32:28 +0200

    add 0-starwars_characters.js

---
+++ starwars_api/0-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}/?format=json`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const characters = JSON.parse(body).characters;
+
+  const printCharacter = (index) => {
+    if (index >= characters.length) {
+      return;
+    }
+    request(characters[index], (err, res, charBody) => {
+      console.log(JSON.parse(charBody).name);
+      printCharacter(index + 1);
+    });
+  };
+
+  printCharacter(0);
+});
